4b5ceef9dde67a679aa280df0424944802c51bca
peilun-he <63130900+peilun-he@users.noreply.github.com>
2020-12-17 14:57:28 +0800

    Bugs fixed

--- Functions/Measurement_Polynomial2.m
+++ Functions/Measurement_Polynomial2.m
@@ -67,35 +67,43 @@ else
     error("Incorrect model. ");
 end
 
-G_est = [0, -lambda_chi, mu_xi-lambda_xi,   sigma_chi^2,                   0,          sigma_xi^2; 
-         0,  -kappa_chi,               0, -2*lambda_chi,     mu_xi-lambda_xi,                   0;
-         0,           0,       -kappa_xi,             0,         -lambda_chi, 2*mu_xi-2*lambda_xi;
-         0,           0,               0,  -2*kappa_chi,                   0,                   0;
-         0,           0,               0,             0, -kappa_chi-kappa_xi,                   0; 
-         0,           0,               0,             0,                   0,         -2*kappa_xi];
+G = [0, -lambda_chi, mu_xi-lambda_xi,   sigma_chi^2,                   0,          sigma_xi^2; 
+     0,  -kappa_chi,               0, -2*lambda_chi,     mu_xi-lambda_xi,                   0;
+     0,           0,       -kappa_xi,             0,         -lambda_chi, 2*mu_xi-2*lambda_xi;
+     0,           0,               0,  -2*kappa_chi,                   0,                   0;
+     0,           0,               0,             0, -kappa_chi-kappa_xi,                   0; 
+     0,           0,               0,             0,                   0,         -2*kappa_xi];
 
 y = zeros(n_point, n_contract); 
-Jy = zeros(n_contract, 2, n_point);
 
-for i = 1: n_point
-    chi = x(1, i);
-    xi = x(2, i);
-    Hx = [1, chi, xi, chi^2, chi * xi, xi^2];
-    exp_matG = zeros(6, 6, n_contract);
-    exp_matG_p = zeros(6, n_contract);
-    
-    C = zeros(1, n_contract);
-    D = zeros(2, n_contract);
-    E = zeros(2, 2, n_contract);
+chi = x(1, :);
+xi = x(2, :);
+Hx = [repelem(1, n_point); chi; xi; chi.^2; chi .* xi; xi.^2]';
 
+if size(mats, 1) == 1 && n_point == 1
+    Jy = zeros(n_contract, 2);
     for j = 1: n_contract
-        exp_matG(:, :, j) = Decomposition_Eigen(mats(i, j)*G);
-        exp_matG_p(:, j) = exp_matG(:, :, j) * p_coordinate;
-        C(j) = exp_matG_p(1, j);
-        D(:, j) = exp_matG_p(2: 3, j);
-        E(:, :, j) = [exp_matG_p(4, j), 0.5*exp_matG_p(5, j); 0.5*exp_matG_p(5, j), exp_matG_p(6, j)];
+        exp_matG = Decomposition_Eigen(mats(:, j)*G);
+        exp_matG_p = exp_matG * p_coordinate;
+        D = exp_matG_p(2: 3, :);
+        E = [exp_matG_p(4, :), 0.5*exp_matG_p(5, :); 0.5*exp_matG_p(5, :), exp_matG_p(6, :)];
         
-        y(i, j) = Hx * exp_matG(:, :, j) * p_coordinate;
-        Jy(j, :, i) = D(:, j) + 2 * E(:, :, j) * x(:, i);    
+        y(:, j) = Hx * exp_matG * p_coordinate;
+        Jy(J,  :) = D + 2 * E * x;   
+    end
+elseif size(mats, 1) == 1 && n_point > 1
+    Jy = 0;
+    for j = 1: n_contract
+        exp_matG = Decomposition_Eigen(mats(:, j)*G);
+        y(:, j) = Hx * exp_matG * p_coordinate;  
+    end
+else
+    Jy = 0;   
+    for i = 1: n_point
+        for j = 1: n_contract
+            exp_matG = Decomposition_Eigen(mats(i, j)*G);
+            y(i, j) = Hx * exp_matG * p_coordinate;
+        end
     end
 end
+
