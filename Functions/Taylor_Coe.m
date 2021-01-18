7f09b57b4b371a1bfa02acf4afd76d7fdd0109b7
peilun-he <63130900+peilun-he@users.noreply.github.com>
2021-01-18 14:46:10 +0800

    Create Taylor_Coe.m

---
+++ Functions/Taylor_Coe.m
@@ -0,0 +1,23 @@
+function [coe] = Taylor_Coe(order)
+
+% Coefficients of Taylor series of f(chi, xi) = exp(chi+xi) at (0, 0)
+% Inputs: 
+%   Order: the order of Taylor series
+% Outputs:
+%   coe: a vector of coefficients
+
+coe = 1;
+
+if order == 0
+  return
+else
+  for s = 1: order
+    for i = 1: s
+      j = s - i;
+      coe = [coe, 1 / (factorial(i) * factorial(j))];
+    end
+  end
+end
+
+
+
