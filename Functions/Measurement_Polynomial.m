b55dee4c212474465298a0c44d2e3f408eb9e91b
peilun-he <63130900+peilun-he@users.noreply.github.com>
2022-02-20 23:35:39 +0800

    Add files via upload

---
+++ Functions/Measurement_Polynomial.m
@@ -0,0 +1,78 @@
+function [y, Jy]= Measurement_Polynomial(x, par, mats, degree, n_coe)
+
+% Polynomial measurement equation
+% Inputs: 
+%   x: x_t
+%   par: a vector of parameters and model coefficients
+%   mats: maturities
+%   degree: degree of polynomial
+%   n_coe: the number of model coefficients
+% Outputs:
+%   y: y_t
+%   Jy: Jacobian 
+
+if n_coe ~= 0
+    par_coe = par(end - n_coe + 1: end); % model coefficients
+    par = par(1: end - n_coe); % model parameters
+end
+
+n_contract = size(mats, 2);
+n_point = size(x, 2); % number of points
+
+if n_coe == (degree+1)*(degree+2)/2
+    p_coordinate = par_coe';
+elseif n_coe == 0
+    p_coordinate = Taylor_Coe(degree)';
+else
+    error("Incorrect number of coefficient. ");
+end
+
+G = GMatrix(par, degree);
+
+y = zeros(n_point, n_contract); 
+
+chi = x(1, :);
+xi = x(2, :);
+Hx = repelem(1, n_point);
+for s = 1: degree
+    for i = s: -1: 0
+        j = s - i;
+        Hx = [Hx; chi.^i .* xi.^j];
+    end
+end
+Hx = Hx';
+
+if size(mats, 1) == 1 && n_point == 1
+    Jy = zeros(n_contract, 2); 
+    for j = 1: n_contract
+        exp_matG = Decomposition_Eigen(mats(:, j)*G);
+        exp_matG_p = exp_matG * p_coordinate;
+        y(:, j) = Hx * exp_matG * p_coordinate;
+        for s = 1: degree
+            for i = s: -1: 0
+                k = s - i;
+                if i ~= 0
+                    Jy(j, 1) = Jy(j, 1) + i .* exp_matG_p(s*(s+1)/2+k+1) .* chi.^(i-1) .* xi.^k;
+                end
+                if k ~= 0
+                    Jy(j, 2) = Jy(j, 2) + k .* exp_matG_p(s*(s+1)/2+k+1) .* chi.^i .* xi.^(k-1);
+                end
+            end
+        end
+    end
+elseif size(mats, 1) == 1 && n_point > 1
+    Jy = 0;
+    for j = 1: n_contract
+        exp_matG = Decomposition_Eigen(mats(:, j)*G);
+        y(:, j) = Hx * exp_matG * p_coordinate;  
+    end
+else
+    Jy = 0;   
+    for i = 1: n_point
+        for j = 1: n_contract
+            exp_matG = Decomposition_Eigen(mats(i, j)*G);
+            y(i, j) = Hx(i, :) * exp_matG * p_coordinate;
+        end
+    end
+end
+
---
+++ Functions/Sandwich.m
@@ -0,0 +1,112 @@
+function [asyVar, message] = Sandwich(par, yt, mats, func_f, func_g, increment, dt, n_coe, model, filter, noise)
+
+% Calculate Sandwich variances of estimates of parameters. 
+% Inputs: 
+%   par: a vector of estimates of parameters
+%   yt: data
+%   mats: time to maturities 
+%   func_f: function f(x), which should take two arguments, xt and a vector of parameters, and return two values, f(x) and f'(x)
+%   func_g: function g(x), which should take three arguments, xt, a vector of parameters and maturities, and return two values, g(x) and g'(x)
+%   increment: the increment to calculate first / second order derivatives numerically
+%`  n_coe: the number of model coefficient to be estimated 
+%   model: SS2000 -> log(S_t) = chi_t + xi_t
+%          PD -> polynomial diffusion model, the degree should be specified by func_g
+%   filter: filtering function
+%   noise: Gaussian -> Gaussian noise for both process and measurement noise
+%          Gamma -> Gaussian process noise and Gamma measurement noise
+% Outputs:
+%   asyVar: asymptotic variance
+%   message: 0 -> normal
+%            1 -> some variances are negative
+
+n_par = length(par);
+[n_obs, ~] = size(yt);
+
+incre = repelem(increment, n_par); % vector of increments
+incre_mat = diag(incre); % matrix of increments
+
+ll_derivative1 = zeros(n_par, n_par, n_obs); % prodcut of first order derivative of log-likelihood 
+ll_derivative2 = zeros(n_par, n_par, n_obs); % second order derivative of log-likelihood
+grad = zeros(n_par, n_obs); % gradient 
+
+for i = 1: n_par
+    i
+    for j = 1: n_par
+        j
+        if i == j
+            if model == "SS2000"
+                [~, ll_table1, ~, ~, ~, ~] = SKF(par + incre_mat(i, :), yt, mats, 0, dt, false, "None"); 
+                [~, ll_table2, ~, ~, ~, ~] = SKF(par, yt, mats, 0, dt, false, "None");
+                [~, ll_table3, ~, ~, ~, ~] = SKF(par - incre_mat(i, :), yt, mats, 0, dt, false, "None");
+            elseif model == "PD"
+                [~, ll_table1, ~, ~] = filter(par + incre_mat(i, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+                [~, ll_table2, ~, ~] = filter(par, yt, mats, func_f, func_g, dt, n_coe, noise); 
+                [~, ll_table3, ~, ~] = filter(par - incre_mat(i, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+            else 
+                error("Incorrect model. ");
+            end
+            
+            ll_derivative2(i, j, :) = (ll_table1 - 2 * ll_table2 + ll_table3) / (incre(i)^2);
+        else            
+            if model == "SS2000"
+                [~, ll_table1, ~, ~, ~, ~] = SKF(par + incre_mat(i, :) + incre_mat(j, :), yt, mats, 0, dt, false, "None"); 
+                [~, ll_table2, ~, ~, ~, ~] = SKF(par + incre_mat(i, :) - incre_mat(j, :), yt, mats, 0, dt, false, "None");
+                [~, ll_table3, ~, ~, ~, ~] = SKF(par - incre_mat(i, :) + incre_mat(j, :), yt, mats, 0, dt, false, "None");
+                [~, ll_table4, ~, ~, ~, ~] = SKF(par - incre_mat(i, :) - incre_mat(j, :), yt, mats, 0, dt, false, "None");
+            elseif model == "PD"
+                [~, ll_table1, ~, ~] = filter(par + incre_mat(i, :) + incre_mat(j, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+                [~, ll_table2, ~, ~] = filter(par + incre_mat(i, :) - incre_mat(j, :), yt, mats, func_f, func_g, dt, n_coe, noise); 
+                [~, ll_table3, ~, ~] = filter(par - incre_mat(i, :) + incre_mat(j, :), yt, mats, func_f, func_g, dt, n_coe, noise); 
+                [~, ll_table4, ~, ~] = filter(par - incre_mat(i, :) - incre_mat(j, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+            else
+                error("Incorrect model. ");
+            end
+
+            ll_derivative2(i, j, :) = (ll_table1 - ll_table2 - ll_table3 + ll_table4) / (4 * incre(i) * incre(j)); 
+        end
+    end
+    
+    if model == "SS2000"
+        [~, ll_table5, ~, ~, ~, ~] = SKF(par + incre_mat(i, :), yt, mats, 0, dt, false, "None"); 
+        [~, ll_table6, ~, ~, ~, ~] = SKF(par - incre_mat(i, :), yt, mats, 0, dt, false, "None");
+    elseif model == "PD"
+        [~, ll_table5, ~, ~] = filter(par + incre_mat(i, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+        [~, ll_table6, ~, ~] = filter(par - incre_mat(i, :), yt, mats, func_f, func_g, dt, n_coe, noise);
+    else
+        error("Incorrect model. ");
+    end
+    
+    grad(i, :) = (ll_table5 - ll_table6) / (2 * incre(i)); 
+end
+
+for i = 1: n_obs
+    ll_derivative1(: ,:, i) = grad(:, i) * grad(:, i)';
+end 
+
+V = sum(ll_derivative1, 3);
+J = sum(ll_derivative2, 3);
+
+asyVar = inv(J) * V * inv(J); 
+
+message = 0;
+
+if any(diag(asyVar) < 0) 
+    message = 1;
+    [V1, D1] = eig(asyVar);
+    for i = 1: n_par
+        if D1(i, i) <= 0
+            D1(i, i) = - D1(i, i); % replace negative variance by its absolute value
+        end
+    end
+    asyVar = V1 * D1 / V1;
+end
+
+
+
+
+
+
+
+
+
+
